ceba0ddd7c1d880201140163df81cb2d48200e7b
Vitor Santos <vcode.santos@gmail.com>
2026-04-21 12:16:37 -0300

    feat: optimize teams carousel for mobile and arrow navigation

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\TeamService;
+use Illuminate\View\View;
+
+class HomeController extends Controller
+{
+    public function __construct(private readonly TeamService $teamService)
+    {
+    }
+
+    public function index(): View
+    {
+        return view('teams-carousel', [
+            'teams' => $this->teamService->getTeams(),
+        ]);
+    }
+}
