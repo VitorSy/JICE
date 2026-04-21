7ba2a97b7b3c273d448b87e8452c5cae5bc31519
Vitor Santos <vcode.santos@gmail.com>
2026-04-21 13:08:15 -0300

    Revert "Add HomeController and TeamService; new homepage and teams carousel views; replace welcome route"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,8 +13,12 @@ class HomeController extends Controller
 
     public function index(): View
     {
-        return view('homepage', [
-            'ranking' => $this->teamService->getMedalRanking(),
+        return view('teams-carousel', [
+            'teams' => $this->teamService->getTeams(),
         ]);
+    }
+
+    public function homepage(): View {
+        return view('home');
     }
 }
